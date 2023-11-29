98b433a31ffded126b22640acaadfc603cd20d91
A.K.M Faisal <akm.faisal@cg-bd.com>
2023-11-29 15:55:32 +0600

    d-money transcation api with newman report

---
+++ dmoneyTransaction.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    //collection: require('./collection/dmoney.json'),
+    collection:`https://api.postman.com/collections/17475087-1f9e9b8f-348e-4e1c-8033-7d561298e248?access_key=${process.env.accessKey}`,
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
